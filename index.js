83eaf895b162249019cf6a16df7bdf3b4603202b
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-10 00:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ function shout(string)
 {
   return string.toUpperCase()
 }
+
+ function whisper(string)
+ {
+   return string.toLowerCase()
+ }
+
+ function logShout(string)
+ {
+   console.log(string.toUpperCase)
+ }
+
+ function logWhisper(string)
+ {
+   console.log(string.toLowerCase()
+ }
+
+ function sayHiToGrandma(string)
+ {
+   if(string.toLowerCase()==string)
+   {
+     return "I cant hear you!!"
+   }
+   if(string.toUpperCase()==string)
+   {
+     return "Yes indeed!"
+   }
+   if(string == "I Love you , Grandma.")
+   {
+     return "I Love you , too."
+   }
+   return "Are you eating enough?"
+ }
